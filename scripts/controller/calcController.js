bb52bd5e5842135a6d1739fed0b89f34deccc18c
Clayton Silva <clayton3660@gmail.com>
2019-07-15 22:49:37 -0300

    Adicionando funcoes para alguns botoes, e exibindo os numeros no visor.

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -20,6 +20,8 @@ class CalController {
             this.setDisplayDateTime();
 
         }, 1000);
+
+        this.setLastNumberToDisplay();
         //setTimeout(() => {
 
           //  clearInterval(interval);
@@ -36,10 +38,12 @@ class CalController {
     }
     clearAll(){
         this._operation = [];
+        this.setLastNumberToDisplay();
     }
 
     clearEntry(){
         this._operation.pop();
+        this.setLastNumberToDisplay();
     }
 
     getLastOperation() {
@@ -57,8 +61,72 @@ class CalController {
         
     }
 
+    pushOperation(value) {
+
+        this._operation.push(value);
+
+        if (this._operation.length > 3) {
+
+
+            this.calc();
+
+            console.log(this._operation);
+        }
+
+    }
+
+    calc() {
+        let last = '';
+
+        if(this._operation.length > 3){
+            let last = this._operation.pop();
+
+        }
+
+        
+        let result = eval(this._operation.join(""));
+
+        if (last == '%') {
+
+            result /= 100;
+
+            this._operation = [result];
+        
+        } else {
+
+            this._operation = [result];
+
+            if (last) this._operation.push(last);
+
+        }
+
+        this.setLastNumberToDisplay();
+
+    }
+
+
+
+    setLastNumberToDisplay(){
+
+        let lastNumber;
+
+        for (let i = this._operation.length-1; i >=0; i--){
+
+
+            if (!this.isOperator(this._operation[i])) {
+                lastNumber = this._operation[i];
+                break;
+            }
+        }
+
+        if (!lastNumber) lastNumber = 0;
+        
+        this.displayCalc = lastNumber;
+
+    }
+
     addOperation(value){
-        console.log('A', isNaN(this.getLastOperation()));
+        //console.log('A', value, isNaN(this.getLastOperation()));
         
         if(isNaN(this.getLastOperation())) {
 
@@ -66,27 +134,37 @@ class CalController {
                 
                 this.setLastOperation(value);
 
-            } else if(isNaN(value)){
+            } else if(isNaN(value)) {
 
-                //Outra coisa
-                console.log(value);
+                console.log('Outra coisa', value);
             
             } else {
-                this._operation.push(value);
 
+                this.pushOperation(value);
+
+                this.setLastNumberToDisplay();
                 
             }
             
 
        
         } else {
-            let newValue = this.getLastOperation().toString() + value.toString();
-            this.setLastOperation(parseInt(newValue));
-        }
 
-        console.log(this._operation);
-        
-        
+            if (this.isOperator(value)) {
+
+                this.pushOperation(value);
+
+            } else {
+
+                let newValue = this.getLastOperation().toString() + value.toString();
+                this.setLastOperation(parseInt(newValue));
+
+
+                this.setLastNumberToDisplay();
+            }
+
+        }
+    
     }
     setError(){
         this.displayCalc = "Error";
@@ -118,7 +196,7 @@ class CalController {
                 this.addOperation('%');   
                 break;
             case 'igual':
-                
+                this.calc();
                 break;
             case 'ponto':
                 this.addOperation('.');
